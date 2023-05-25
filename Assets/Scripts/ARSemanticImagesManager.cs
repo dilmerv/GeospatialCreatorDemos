734234c4d9410a97233be474fb783356313c2762
Dilmer Valecillos <dilmervst@gmail.com>
2023-05-25 14:46:19 -0600

    Initial demo with semantic segmentation

---
+++ Assets/Scripts/ARSemanticImagesManager.cs
@@ -0,0 +1,50 @@
+using Google.XR.ARCoreExtensions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARSemanticImagesManager : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage semanticRawImage;
+
+    private Texture2D semanticTexture2D;
+
+    private ARSemanticManager semanticManager;
+
+    private bool semanticTextureObtained;
+
+    private bool isSemanticModeAvailable;
+
+    private void Awake()
+    {
+        semanticManager = FindObjectOfType<ARSemanticManager>();
+
+        // check for support
+        if(semanticManager.IsSemanticModeSupported(SemanticMode.Enabled) == FeatureSupported.Supported)
+        {
+            Logger.Instance.LogWarning("Unfortunately Semantic Segmentation is not supported on this device");
+        }
+        else
+        {
+            Logger.Instance.LogWarning("Semantic Segmentation is supported on this device");
+            isSemanticModeAvailable = true;
+        }
+    }
+
+    void Update()
+    {
+        if (!isSemanticModeAvailable) return;
+
+        if (semanticManager.TryGetSemanticTexture(ref semanticTexture2D))
+        {
+            semanticRawImage.texture = semanticTexture2D;
+            semanticTextureObtained = true;
+        }
+        else
+        {
+            semanticTextureObtained = false;
+        }
+
+        Logger.Instance.LogInfo($"Person probability in view: {semanticManager.GetSemanticLabelFraction(SemanticLabel.Person)}");
+    }
+}
